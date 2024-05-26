88f0c04d473c3a06a7ee1eddec870ca5ed58994c
weinhooney <devweinhooney@gmail.com>
2024-05-26 22:58:42 +0900

    동적 배열 구현 삭제

--- Algorithm/Board.cs
+++ Algorithm/Board.cs
@@ -6,55 +6,9 @@ using System.Threading.Tasks;
 
 namespace Algorithm
 {
-    class MyList<T>
-    {
-        const int DEFAULT_SIZE = 1;
-        T[] _data = new T[DEFAULT_SIZE];
-
-        public int Count = 0; // 실제로 사용중인 데이터 개수
-        public int Capacity { get { return _data.Length; } } // 예약된 데이터 개수
-
-        public void Add(T item)
-        {
-            // 1. 공간이 충분히 남아 있는지 확인한다
-            if(Count >= Capacity)
-            {
-                // 공간을 다시 늘려서 확보한다
-                T[] newArray = new T[Count * 2];
-                for(int i = 0; i < Count; ++i)
-                {
-                    newArray[i] = _data[i];
-                }
-                _data = newArray;
-            }
-
-            // 2. 공간에다가 데이터를 넣어준다
-            _data[Count] = item;
-            Count++;
-        }
-
-        public T this[int index]
-        {
-            get { return _data[index]; }
-            set { _data[index] = value; }
-        }
-
-        public void RemoveAt(int index)
-        {
-            for(int i = index; i < Count - 1; ++i)
-            {
-                _data[i] = _data[i + 1];
-            }
-
-            _data[Count - 1] = default(T);
-            Count--;
-        }
-    }
-
     class Board
     {
         public int[] _data = new int[25];
-        public MyList<int> _data2 = new MyList<int>();
         public LinkedList<int> _data3 = new LinkedList<int>();
 
         public void Initialize()
