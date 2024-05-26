8ed736dd3120ceb626562925460fd86961807953
weinhooney <devweinhooney@gmail.com>
2024-05-27 01:40:13 +0900

    미로 외곽 생성

--- Algorithm/Board.cs
+++ Algorithm/Board.cs
@@ -8,11 +8,67 @@ namespace Algorithm
 {
     class Board
     {
-        public int[] _data = new int[25];
+        const char CIRCLE = '\u25cf';
+        public TileType[,] _tile;
+        public int _size;
 
-        public void Initialize()
+        public enum TileType
         {
+            Empty,
+            Wall,
+        }
 
+        public void Initialize(int size)
+        {
+            _tile = new TileType[size, size];
+            _size = size;
+
+            for(int y = 0; y < _size; ++y)
+            {
+                for(int x = 0; x < _size; ++x)
+                {
+                    if(x == 0 || x == _size - 1 || y == 0 || y == size - 1)
+                    {
+                        _tile[y, x] = TileType.Wall;
+                    }
+                    else
+                    {
+                        _tile[y, x] = TileType.Empty;
+                    }
+                }
+            }
+        }
+
+        public void Render()
+        {
+            ConsoleColor prevColor = Console.ForegroundColor;
+
+            for (int y = 0; y < _size; ++y)
+            {
+                for (int x = 0; x < _size; ++x)
+                {
+                    Console.ForegroundColor = GetTileColor(_tile[y, x]);
+                    Console.Write(CIRCLE);
+                }
+                Console.WriteLine();
+            }
+
+            Console.ForegroundColor = prevColor;
+        }
+
+        ConsoleColor GetTileColor(TileType type)
+        {
+            switch(type)
+            {
+                case TileType.Empty:
+                    return ConsoleColor.Green;
+
+                case TileType.Wall:
+                    return ConsoleColor.Red;
+
+                default:
+                    return ConsoleColor.Green;
+            }
         }
     }
 }
--- Algorithm/Program.cs
+++ Algorithm/Program.cs
@@ -11,12 +11,11 @@ namespace Algorithm
         static void Main(string[] args)
         {
             Board board = new Board();
-            board.Initialize();
+            board.Initialize(25);
 
             Console.CursorVisible = false;
 
             const int WAIT_TICK = 1000 / 30;
-            const char CIRCLE = '\u25cf';
 
             int lastTick = 0;
             while(true)
@@ -36,17 +35,9 @@ namespace Algorithm
                 // 로직
                 // 렌더링
                 Console.SetCursorPosition(0, 0);
+                board.Render();
                 
-                for(int i = 0; i < 25; ++i)
-                {
-                    for(int j = 0; j < 25; ++j)
-                    {
-                        Console.ForegroundColor = ConsoleColor.Green;
-                        Console.Write(CIRCLE);
-                    }
-                    Console.WriteLine();
 
-                }
             }
         }
     }
